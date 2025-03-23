8ef9a2c3d5d4f336dfa4d0d4dfa3dfd806da04b4
lothsun <lothsun@yahoo.com>
2025-03-23 01:35:20 -0500

    Debug Logging
    Added some extra data to the debug logs for future things and troubleshooting. Also updated the loaded successfully message to show the correct 3.1 version of the mod.

--- scripts/5_mission/mission/geb_missionserver.c
+++ scripts/5_mission/mission/geb_missionserver.c
@@ -4,7 +4,7 @@ modded class MissionServer
 	{		
 		super.OnInit();
 		if(m_gebsConfig){
-			Print("[gebsfish] Gebsfish V3.0 Loaded Successfully!");
+			Print("[gebsfish] Gebsfish V3.1 Loaded Successfully!");
 		}
 		
 	}
@@ -33,9 +33,62 @@ modded class MissionServer
 			int count = mGeb_YieldsMapAll.Count();
 			for (int i = 0; i < count; i++){
 				yItem = mGeb_YieldsMapAll.GetElement(i);
-				Print("[gebsfish] [DEBUG] [YieldMapOutput] Yield Item: " + yItem);
-				Print("[gebsfish] [DEBUG] [YieldMapOutput] Yield Item Type: " + yItem.GetType());
-				Print("[gebsfish] [DEBUG] [YieldMapOutput] Yield Item Name: " + GetDisplayNameFromTypeName(yItem.GetType()));
+				string gebDebugCatchMethod;
+				string gebDebugCatchEnviro;
+				switch(yItem.GetEnviroMask()) {
+					case 1:
+						gebDebugCatchEnviro = "Pond";
+						break;
+					case 2: 
+						gebDebugCatchEnviro = "Sea";
+						break;
+					case 3: 
+						gebDebugCatchEnviro = "Pond and Sea";
+						break;
+					case 4: 
+						gebDebugCatchEnviro = "Forest";
+						break;
+					case 8:
+						gebDebugCatchEnviro = "Field";
+						break;
+					default: 
+						gebDebugCatchEnviro = "Environment Unknown or Out of Range";
+						break;
+				}
+				switch(yItem.GetMethodMask()) {
+					case 1:
+						gebDebugCatchMethod = "Rod";
+						break;
+					case 2: 
+						gebDebugCatchMethod = "Large Trap";
+						break;
+					case 3: 
+						gebDebugCatchMethod = "Rod and Large Trap";
+						break;
+					case 4:
+						gebDebugCatchMethod = "Small Trap";
+						break;
+					case 5: 
+						gebDebugCatchMethod = "Rod and Small Trap";
+						break;
+					case 6: 
+						gebDebugCatchMethod = "Large Trap and Small Trap";
+						break;
+					case 7: 
+						gebDebugCatchMethod = "Rod, Large Trap, and Small Trap";
+						break;
+					case 8:
+						gebDebugCatchMethod = "Land Trap: Snare";
+						break;
+					default: 
+						gebDebugCatchMethod = "Catch Method Unknown or Out of Range";
+						break;
+				}
+				Print("[gebsfish] [DEBUG] [YieldMapOutput] Yield Item " + i + " : " + yItem);
+				Print("[gebsfish] [DEBUG] [YieldMapOutput] Yield Item " + i + " Type: " + yItem.GetType());
+				Print("[gebsfish] [DEBUG] [YieldMapOutput] Yield Item " + i + " Name: " + GetDisplayNameFromTypeName(yItem.GetType()));
+				Print("[gebsfish] [DEBUG] [YieldMapOutput] Yield Item " + i + " Catch Method: " + gebDebugCatchMethod);
+				Print("[gebsfish] [DEBUG] [YieldMapOutput] Yield Item " + i + " Catch Environment: " + gebDebugCatchEnviro);
 			}
 
 			Print("[gebsfish] [DEBUG] [YieldMapOutput] End YieldMapAll Dump");
