092c24adec1410a503f3151c33de0289cc3dd16e
Lothsun <lothsun@yahoo.com>
2025-06-13 18:58:09 -0500

    Update constants.c
    Added Config Version constant to reference in the rest of the mod.

--- scripts/3_game/constants.c
+++ scripts/3_game/constants.c
@@ -3,3 +3,7 @@ const int RPC_PLAY_PREDATOR_SOUND = 2757509117; // Unique ID for the predator so
 
 // Define RPC constant for Config Sync
 const int RPC_GEBSCONFIG_SYNC = 2757509118; // Unique ID for the predator sound RPC
+
+//Define Config Version Constant
+
+const str VERSION_GEBSFISH = "3.3"; // Current version of the mod 
