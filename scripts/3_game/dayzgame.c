70f45e2e5a3368f8d8c006b59c1704365cfea631
Cole <34726562+PackJC@users.noreply.github.com>
2026-03-16 01:44:59 -0400

    Remove invalid `ref` keywords from RPC handler parameters
    Enforce Script does not allow strong references on method arguments.
    Updated ConfigSync and PlayPredatorSound RPC signatures to match
    Community Framework expectations and eliminate script warnings.

--- scripts/3_game/dayzgame.c
+++ scripts/3_game/dayzgame.c
@@ -1,51 +1,51 @@
-modded class DayZGame {
-    
+modded class DayZGame
+{
     override void DeferredInit()
     {
         super.DeferredInit();
-        
+
         // Register RPCs with Community Framework - must be done on both client and server
         GetRPCManager().AddRPC("gebsfish", "ConfigSync", this, SingleplayerExecutionType.Client);
         GetRPCManager().AddRPC("gebsfish", "PlayPredatorSound", this, SingleplayerExecutionType.Client);
     }
-    
-    void ConfigSync(CallType type, ref ParamsReadContext ctx, ref PlayerIdentity sender, ref Object target)
+
+    void ConfigSync(CallType type, ParamsReadContext ctx, PlayerIdentity sender, Object target)
     {
         GebsfishLogger.Info("ConfigSync RPC callback called. Type: " + type + ", IsClient: " + g_Game.IsClient(), "RPC");
-        
+
         if (type != CallType.Client)
         {
             GebsfishLogger.Info("ConfigSync: Not client type, returning.", "RPC");
             return;
         }
-        
+
         Param1<gebsfishConfig> configParams;
-        if(!ctx.Read(configParams))
+        if (!ctx.Read(configParams))
         {
             GebsfishLogger.Error("ConfigSync: Failed to read configParams from context!", "RPC");
             return;
         }
-        
+
         GebsfishLogger.Info("ConfigSync: Successfully read configParams, setting config.", "RPC");
         SetGebsfishConfig(configParams.param1);
         GebsfishLogger.Info("Client received config data " + VERSION_GEBSFISH + " from the server.", "RPC");
     }
-    
-    void PlayPredatorSound(CallType type, ref ParamsReadContext ctx, ref PlayerIdentity sender, ref Object target)
+
+    void PlayPredatorSound(CallType type, ParamsReadContext ctx, PlayerIdentity sender, Object target)
     {
         if (type != CallType.Client)
             return;
-        
+
         Param1<string> data;
         if (!ctx.Read(data))
             return;
-        
+
         // Use Man instead of PlayerBase since Man is accessible from 3_Game module
         // PlaySoundSet is available on Man/ManBase which PlayerBase extends
         Man player = Man.Cast(target);
         if (!player)
             return;
-        
+
         string soundSetName = data.param1;
         EffectSound soundEffect;
         GebsfishLogger.Debug("Received RPC to play sound: " + soundSetName + ".", "PredatorSpawnFishingRPC");
